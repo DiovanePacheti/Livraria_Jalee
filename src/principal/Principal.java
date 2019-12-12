01a3c393123afb0186f3b05dabeea2dc7fe17f29
DiovanePacheti <frontendestudo@gmail.com>
2019-12-11 23:52:00 -0300

    começando

---
+++ src/principal/Principal.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author dio-end
+ */
+public class Principal {
+    public static void main(String[] args) {
+        
+    }//fim do method main
+}//fim da class Principal
